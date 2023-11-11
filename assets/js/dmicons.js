7fbcfdcee46730ac7bbb00a8f410573075b76fbf
Daniel Melin <daniel.melin@gmail.com>
2023-11-11 01:57:46 +0100

    swapped alpha for latest

--- assets/js/dmicons.js
+++ assets/js/dmicons.js
@@ -1,4 +1,4 @@
-const basePath = "https://cdn.jsdelivr.net/gh/dmelin/icons@alpha/"
+const basePath = "https://cdn.jsdelivr.net/gh/dmelin/icons@latest/"
 console.log("DM-Icons JS loaded")
 document.addEventListener('DOMContentLoaded', function () {
     console.log("started")
